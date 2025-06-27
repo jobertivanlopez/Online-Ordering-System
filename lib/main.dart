94d359016dc5d166a2968aec38187a065defe1e0
jemarc27 <delarosajemarc3@gmail.com>
2025-06-27 11:40:51 +0800

    WEEK1 DONE

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,22 @@
 import 'package:flutter/material.dart';
+import 'pages/landingPage.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HeroSection(),
-    debugShowCheckedModeBanner: false,
-
-  ));
+  runApp(const MyApp());
 }
 
-class HeroSection extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 40.0),
-          color: Colors.white,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // Red italic text
-              Text(
-                'Satisfy your cravings',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontStyle: FontStyle.italic,
-                  color: Colors.redAccent,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              SizedBox(height: 16),
-
-              // Bold black text
-              Text(
-                'Feel the warmth.\nTaste the flavor.\nLomi at your doorstep.',
-                style: TextStyle(
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black87,
-                  height: 1.4,
-                ),
-              ),
-              SizedBox(height: 32),
-
-              // Order Now button
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFFFD766),
-                  foregroundColor: Colors.black,
-                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                ),
-                child: Text(
-                  'Order Now',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+    return MaterialApp(
+      title: 'LomiHub',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/landingPage',
+      routes: {
+        '/landingPage': (context) => const  LandingPage(),
+      },
     );
   }
 }
